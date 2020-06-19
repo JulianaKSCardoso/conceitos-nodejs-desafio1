22f5b73482a4fe3c77f370a39eacfc6c0d2e30eb
JulianaKSCardoso <50932720+JulianaKSCardoso@users.noreply.github.com>
2020-06-18 22:59:49 -0300

    Add files via upload
    Resolução do exercício

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4');
 
 const app = express();
 
@@ -10,24 +10,64 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function doesIdExist(request, response, next) {
+    const { id } = request.params;
+
+    const idExists = repositories.find((repo) => repo.id === id);
+
+    if (!idExists) {
+        return response.status(400).json({ error: 'Repository not found' });
+    }
+
+    return next();
+}
+
+app.get('/repositories', (request, response) => response.json(repositories));
+
+app.post('/repositories', (request, response) => {
+    const { title, url, techs } = request.body;
+
+    const repository = {
+        id: uuid(),
+        title,
+        url,
+        techs,
+        likes: 0,
+    };
+
+    repositories.push(repository);
+
+    return response.json(repository);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.put('/repositories/:id', doesIdExist, (request, response) => {
+    const { title, url, techs } = request.body;
+    const { id } = request.params;
+
+    const repository = repositories.find((repo) => repo.id === id);
+    repository.title = title;
+    repository.url = url;
+    repository.techs = techs;
+
+    return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.delete('/repositories/:id', doesIdExist, (request, response) => {
+    const { id } = request.body;
+
+    const repository = repositories.find((repo) => repo.id === id);
+    repositories.pop(repository);
+
+    return response.status(204).send();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.post('/repositories/:id/like', doesIdExist, (request, response) => {
+    const { id } = request.params;
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+    const repository = repositories.find((repo) => repo.id === id);
+    repository.likes += 1;
+
+    return response.json(repository);
 });
 
 module.exports = app;
